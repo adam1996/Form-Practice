eda83d7c8e9e6ad289a2e6e07779a210218d1386
Adam Johnson <adam7819@googlemail.com>
2023-07-24 18:37:58 +0100

    Correct input type to 'password'

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Load elements from DOM: 
+/*Load elements from DOM: */
 const form = document.getElementById('form');
 const username = document.getElementById('username');
 const email = document.getElementById('email');
@@ -20,9 +20,13 @@ const showSuccess = (input) => {
 }
 
 //Verify email: 
-const isValidEmail = (email) => {
+const checkEmail = (input) => {
     const reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    return reg.test(email);
+    if (reg.test(input.value.trim())) {
+        showSuccess(input);
+    } else {
+        showError(input, 'Email is not valid');
+    }
 }
 
 //Check required fields
@@ -36,6 +40,22 @@ const checkRequired = (inputArr) => {
     });
 }
 
+//Check input lengths
+const checkLength = (input, min, max) => {
+    if(input.value.length < min){
+        showError(input, `${getFieldName(input)} must be at least ${min} characters.`);
+    } else if (input.value.length > max){
+        showError(input, `${getFieldName(input)} must be less than ${max} characters.`);
+    } else {
+        showSuccess(input);
+    }
+}
+//Check password: 
+const checkPasswordMatch = (p1, p2) => {
+    if (p1.value !== p2.value){
+        showError(p2, 'Passwords do not match');
+    }
+}
 const getFieldName = (input) => {
     return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
@@ -46,4 +66,8 @@ form.addEventListener('submit', function(e){
     e.preventDefault(); //prevents the form from submitting
 
     checkRequired([username, email, password, password2]);
+    checkLength(username, 3, 15);
+    checkLength(password, 6, 25);
+    checkEmail(email);
+    checkPasswordMatch(password, password2);
 })
